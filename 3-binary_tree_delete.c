cf37816b55781f49a343cdf239429f0cd2b9ccbf
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-06-05 13:09:23 -0700

    Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "bianry_trees.h"
+
+/*
+ * binary_tree_delete - a function that deletes an entire
+ * binary tree
+ * @tree: a pointer to the root node to delete
+ * Return: return nothing
+ * */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	else
+		free(tree);
+}

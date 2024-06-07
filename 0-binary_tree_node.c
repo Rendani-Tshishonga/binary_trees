b5480d0103a58b526f58408c8615bba5a6c2cd56
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-06-07 03:51:47 -0700

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,9 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newnode;
 
 	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (root == NULL)
+	if (newnode == NULL)
 	{
-		return NULL
+		return NULL;
 	}
 	newnode->n = value;
 	newnode->parent = parent;
--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,22 @@
 #ifndef _BINARY_TREES_H
 #define _BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct binary_tree_s
+{
+        int n;
+        struct binary_tree_s *parent;
+        struct binary_tree_s *left;
+        struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+/**binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
@@ -11,14 +25,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
-
-struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
+*/
 
 #endif
